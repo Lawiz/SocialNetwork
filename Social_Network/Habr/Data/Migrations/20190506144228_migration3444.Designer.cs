7b0325b2d949b5c136fa7f9cd080a8982bec92f4
Petr <p.sobolev@cbt.by>
2019-05-06 17:44:15 +0300

    migration new

--- Social_Network/Habr/Data/Migrations/20190506144228_migration3444.Designer.cs
+++ Social_Network/Habr/Data/Migrations/20190506144228_migration3444.Designer.cs
@@ -4,14 +4,16 @@ using Habr.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Habr.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190506144228_migration3444")]
+    partial class migration3444
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Social_Network/Habr/Data/Migrations/20190506144228_migration3444.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Habr.Data.Migrations
+{
+    public partial class migration3444 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
